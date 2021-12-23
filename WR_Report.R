9ac46b1a81b555debe9d4b68b566b4ce4615d4c2
Danilo Ribeiro <danilocbjr@gmail.com>
2021-12-23 17:16:03 -0300

    V1.0

---
+++ WR_Report.R
@@ -0,0 +1,32 @@
+#o objetivo desse script é exercitar os conhecimentos adquiridos na matéria de Data Wrangling do
+#curso de MBA DSA USP/ESALQ
+
+
+#instalando os pacotes necessários
+
+install.packages("tidyverse")
+library("tidyverse")
+library(readxl)
+
+#importando os arquivos em excel
+
+ob_init <- read_excel("Backlog 17.12.xlsx")
+fat_init <- read_excel("Faturamento 17.12.xlsx")
+
+#limpando o dataset: tirando as linhas 1:5, depois transformando a 6º linha em texto e,
+#depois, em cabeçalho e apagando a sexta linha (que agora é a primeira) e colunas que
+#não são usadas, além de melhoria nos títulos das variáveis
+
+ob_2 <- ob_init[-c(1:5),]
+names(ob_2) <- as.character(ob_2[1,])
+ob_2 <- ob_2[-1,]
+
+#fazendo a mesma limpeza para o arquivo de faturamento
+
+fat_2 <- fat_init[-c(1:5),]
+names(fat_2) <- as.character(fat_2[1,])
+fat_2 <- fat_2[-1,]
+
+#Agora, temos que criar um arquivo em excel com o seguinte detalhe:
+#1. 
+
